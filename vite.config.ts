b352a6d62f58273da9787da27f5bc24060361e03
kreatweb <oreoglaze07@gmail.com>
2026-05-24 15:43:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/kreatweb/",   
+  base: "/kreatweb-ed709e88/",   
 
   server: {
     host: "::",
